f550b1b55f14418b263637d35a1b7bf38e2bd087
Alex Chi <iskyzh@gmail.com>
2020-02-18 00:02:43 +0800

    batch analysis now automatically output two types of value

--- map-visualization/src/batch_analysis.js
+++ map-visualization/src/batch_analysis.js
@@ -5,23 +5,23 @@ function xyToLongLat(x, y) {
     return [-179.95 + 0.1 * nx, 89.95 - 0.1 * ny]
 }
 
-const r = 80000
+const r = 70000
 
-function analyze(m) {
+const isInRange = (x, y) => {
+    const pB = xyToLongLat(563, 310)   
+    const pA = xyToLongLat(x, y)
+    return geo.pointDistance(pA, pB, true)
+}
+const newInRange = (x, y) => {
+    const pB = xyToLongLat(592, 239)   
+    const pA = xyToLongLat(x, y)
+    return geo.pointDistance(pA, pB, true)
+}
+
+function analyze(m, ran, loc) {
     const base = require('./base')
     const data1 = require(`${base}/2050-${m}.json`).fish
     const data2 = require(`${base}/2051-${m}.json`).fish
-    const isInRange = (x, y) => {
-        const pB = xyToLongLat(563, 310)   
-        const pA = xyToLongLat(x, y)
-        return geo.pointDistance(pA, pB, true)
-    }
-    const newInRange = (x, y) => {
-        const pB = xyToLongLat(592, 239)   
-        const pA = xyToLongLat(x, y)
-        return geo.pointDistance(pA, pB, true)
-    }
-    const ran = newInRange
     let sumdist = 0
     let n = 0
     data1.forEach(f => {
@@ -38,9 +38,14 @@ function analyze(m) {
             n += 1
         }
     })
-    console.log(`${sumdist / n},${n},New,${m},Herring`)
+    console.log(`${sumdist / n},${n},${loc},${m},Herring`)
 }
 
+console.log(xyToLongLat(1130, 540))
+
 for (let i = 1; i <=12; i++) {
-    analyze(i)
+    analyze(i, isInRange, "Original")
+}
+for (let i = 1; i <=12; i++) {
+    analyze(i, newInRange, "New")
 }
